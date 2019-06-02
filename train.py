e4cf3496644a85701a0d343a7913f2764e4b074f
Kamal Rahimi Malekshan <rahimi.kamal@gmail.com>
2019-06-02 15:44:50 -0400

    Code cleanup and comments

--- train.py
+++ train.py
@@ -38,13 +38,14 @@ FIW_DATA_FILE_PATH = "./data/family_in_wild.pickle"
 
 
 def prepare_data(X_train, X_test):
-    """ Prepares training and test data for emotion detection model from Family in Wild (FIW)
+    """ Prepares training and test data for family relations detection model from Family in Wild (FIW)
     image dataset 
     Args:
-        dataset: an object of the FamilyInWildDataset class
+        X_train: a numpy array of the train image data
+        X_test: a numpy array of the test image data
     Returns:
-        X_train: a numpy array of the train face image data
-        X_test: a numpy array of the test face image data
+        X_train: a numpy array of the train image data
+        X_test: a numpy array of the test image data
     """    
     X_train = np.array(X_train).astype('float32')
     X_test  = np.array(X_test).astype('float32')
@@ -59,8 +60,6 @@ def create_relation_encoder_model(X_train, X_test):
     Args:
         X_train: a numpy array of the train image data
         X_test: a numpy array of the test image data
-        y_train: a numpy array of the train emotion lables
-        y_test: a numpy array of the test emotion lables
     Returns:
     """
     input_image = Input(shape=(image_height, image_width, image_n_channels))
@@ -91,10 +90,10 @@ def create_relation_encoder_model(X_train, X_test):
     return autoencoder
 
 def prepare_relation_info(relation_dict, y):
-    """ Prepares training and test data for emotion detection model from Family in Wild (FIW)
-    image dataset 
+    """ Prepares training and test data for emotion detection model
     Args:
-        dataset: an object of the FamilyInWildDataset class
+        relation_dict: a Python dictionary of people related to a person
+        y: a numpy array conatining labels of each image (family_id/person_id)
     Returns:
         X_related_dict: a Python dictionary of image indices related to an index
         X_not_related_dict: a Python dictionary of image indices not related to an index
